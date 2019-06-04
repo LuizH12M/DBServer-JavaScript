d1f06a89608881803dfaaa39dc51292dcf345982
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-06-04 16:37:17 -0300

    Primeiro Exercicio

---
+++ Exercicio_1/alo.js
@@ -0,0 +1,4 @@
+var umaPessoa;
+umaPessoa = 'John Doe';
+var outraPessoa = 'Mary'; // Inferencia de tipo String
+document.body.innerHTML = "Al\u00F4, " + umaPessoa;
---
+++ Exercicio_1/alo.ts
@@ -0,0 +1,10 @@
+let umaPessoa: string;
+umaPessoa = 'John Doe';
+
+let outraPessoa = 'Mary'; // Inferencia de tipo String
+
+document.body.innerHTML = `Alô, ${umaPessoa}`;
+
+
+
+
