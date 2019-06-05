64c18d3a595b96b9caa6ad98f2332f0d09e02e3b
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-06-05 14:17:27 -0300

    Exercicio Finalizado

--- Exercicio_1/alo.js
+++ Exercicio_1/alo.js
@@ -2,4 +2,8 @@ var umaPessoa;
 umaPessoa = 'John Doe';
 var outraPessoa = 'Mary'; // Inferencia de tipo String
 var hoje = new Date();
-document.body.innerHTML = "Al\u00F4, " + umaPessoa + " ! Hoje \u00E9 " + hoje.toDateString();
+// document.body.innerHTML = `Alô, ${umaPessoa} ! Hoje é ${hoje.toDateString()}`;
+document.body.innerHTML = alo(outraPessoa);
+function alo(nome) {
+    return 'Alô, ' + nome;
+}
--- Exercicio_1/alo.ts
+++ Exercicio_1/alo.ts
@@ -4,8 +4,12 @@ umaPessoa = 'John Doe';
 let outraPessoa = 'Mary'; // Inferencia de tipo String
 
 let hoje = new Date();
-document.body.innerHTML = `Alô, ${umaPessoa} ! Hoje é ${hoje.toDateString()}`;
+// document.body.innerHTML = `Alô, ${umaPessoa} ! Hoje é ${hoje.toDateString()}`;
 
+document.body.innerHTML = alo(outraPessoa);
 
+function alo(nome: string): string {
+    return 'Alô, ' + nome;
+}
 
 
