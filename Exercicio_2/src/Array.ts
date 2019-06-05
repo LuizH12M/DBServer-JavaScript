0528e21c92060aadb4a8b29d72e0826b79f208e6
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-06-05 17:12:09 -0300

    Mais organizado e Map

--- Exercicio_2/src/Array.ts
+++
@@ -1,9 +0,0 @@
-let palavras : string [] = ['a', 'b'];
-console.log(palavras.pop());
-console.log(palavras.pop());
-console.log(palavras.pop()); // retorna undefined
-
-// Variavel X é do tipo Number OU Undefined
-let x : number | undefined;
-x = 1;
-x = undefined;
