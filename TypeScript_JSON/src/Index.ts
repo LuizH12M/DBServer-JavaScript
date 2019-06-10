e5fc104dfbc64a7204ccd7c7d5672e61c92b4195
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-06-10 14:49:34 -0300

    Funcoes assincronas

--- TypeScript_JSON/src/Index.ts
+++ TypeScript_JSON/src/Index.ts
@@ -9,8 +9,9 @@ class Pessoa {
 }
 
 let umaPessoa = new Pessoa ('John Doe', 22);
-let json = JSON.stringify(umaPessoa);
+let json = JSON.stringify(umaPessoa); // JSON é o formato de um objeto
+// STRINGFY faz um objeto virar string
 console.log(json);
 let jsonstring = '{"nome" : "John Doe", "idade":22}';
-let obj = JSON.parse(jsonstring);
+let obj = JSON.parse(jsonstring); // PARSE faz virar um objeto de novo
 console.log(obj);
--- TypeScript_Lab003/src/Index.ts
+++ TypeScript_Lab003/src/Index.ts
@@ -87,6 +87,9 @@ class Moeda {
     get nome(): string {
         return this._nome;
     }
+
+    setValor (v : number)
+    { this._valor = v; }
 }
 
 class Cofrinho {
@@ -101,12 +104,47 @@ class Cofrinho {
         //                       tipo                                   função
         return this.moedas.reduce(somador, 0);
     }
+
+    getMoeda() 
+    { return this.moedas; }
+// ------------------------------------------------------------------
+// Exercício 3:
+    menorValor() : number {
+        let menor = this.getMoeda()[0].valor;
+        for (let i = 1; i <= Moeda.length; i++) {
+            let valorMoedaAtual = this.getMoeda()[i].valor;
+            if (menor > valorMoedaAtual) {
+                menor = valorMoedaAtual;
+            }
+        }
+        return menor;
+    }
+
+    menorMoeda() : string {
+        let a : number = 0;
+        for(let i = 1; i <= Moeda.length; i++) {
+            if(this.moedas[i].valor === cofre.menorValor()) {
+                 a = i;
+            }
+        }
+        return this.moedas[a].nome + ' ' + this.moedas[a].valor;
+    }
+
+    //frequenciaMoedas() : Map<number,string> {
+        
+    //}
+
+
 }
 let moeda1: Moeda = new Moeda(50, "50 centavos");
 let moeda2: Moeda = new Moeda(25, "25 centavos");
+let moeda3: Moeda = new Moeda(1, "1 centavo");
 let cofre: Cofrinho = new Cofrinho();
 cofre.adicionar(moeda1);
 cofre.adicionar(moeda2);
-console.log(cofre.calcularTotal());
+cofre.adicionar(moeda3);
+//console.log(cofre.calcularTotal());
+console.log('Menor valor: ' + cofre.menorValor());
+console.log('Menor moeda: ' + cofre.menorMoeda());
 
 
