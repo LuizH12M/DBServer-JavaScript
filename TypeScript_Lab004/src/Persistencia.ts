1ec32337089c60a5f37daff3e736122a559db41d
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-06-11 18:20:38 -0300

    Lab 004 finalizado!

--- TypeScript_Lab004/src/Persistencia.ts
+++ TypeScript_Lab004/src/Persistencia.ts
@@ -10,28 +10,28 @@ export async function salvarCofrinho(cofre: Cofrinho, nomeArquivo: string) {
     }
 }
 
-// export function lerCofrinho(nomeArquivo: string): Promise<Cofrinho> {
-//     return fs.promises.readFile(nomeArquivo, 'utf-8')
-//     .then(dados => JSON.parse(dados))
-//     .then(obj => {
-//         const cofre = new Cofrinho();
-//         for (let i=0; i<obj.moedas.lenght; i++) {
-//             cofre.adicionar(new Moeda(obj.moedas[i].valor, obj.moedas[i].nome));
-//         }
-//         return cofre;
-//     });
-// }
-
-export async function lerCofrinho(nomeArquivo: string): Promise<Cofrinho> {
-    const dados = await promises.readFile(nomeArquivo, 'utf-8');
-    try {
-        const obj = JSON.parse(dados);
+export function lerCofrinho(nomeArquivo: string): Promise<Cofrinho> {
+    return fs.promises.readFile(nomeArquivo, 'utf-8')
+    .then(dados => JSON.parse(dados))
+    .then(obj => {
         const cofre = new Cofrinho();
         for (let i=0; i<obj.moedas.lenght; i++) {
-            cofre.adicionar(new Moeda(obj.moedas[i]._valor, obj.moedas[i]._nome));
-        } 
+            cofre.adicionar(new Moeda(obj.moedas[i].valor, obj.moedas[i].nome));
+        }
         return cofre;
-    } catch (erro) {
-        throw erro;
-    }
+    });
 }
+
+// export async function lerCofrinho(nomeArquivo: string): Promise<Cofrinho> {
+//     const dados = await promise.readFile(nomeArquivo, 'utf-8');
+//     try {
+//         const obj = JSON.parse(dados);
+//         const cofre = new Cofrinho();
+//         for (let i=0; i<obj.moedas.lenght; i++) {
+//             cofre.adicionar(new Moeda(obj.moedas[i]._valor, obj.moedas[i]._nome));
+//         } 
+//         return cofre;
+//     } catch (erro) {
+//         throw erro;
+//     }
+// }
