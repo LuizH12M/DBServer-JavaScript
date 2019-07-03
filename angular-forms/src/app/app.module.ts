bfd87b9f84f4e814780ac258957823c38a8b8fa4
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-07-03 14:31:40 -0300

    Forms + Lab 08

---
+++ angular-forms/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { Cadastro2Component } from './cadastro2/cadastro2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent,
+    Cadastro2Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
